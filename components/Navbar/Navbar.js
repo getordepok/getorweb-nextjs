60712c799bc0440ac5d5a4e4c8ab9ec511cf5fb0
juanbiantong <jbiantong@gmail.com>
2022-06-18 17:59:57 +0700

    add button menu

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,21 +1,40 @@
 import React from "react";
+import Button from "../Button";
+import Logo from "./Logo";
+import Nav from "./Nav";
 
-function Navbar() {
+export default function Navbar() {
   return (
     <>
-      <nav id="navbar" className="flex justify-around items-center">
-        <div className="flex items-center">
-          <img
-            className="h-14 items-center"
-            src="/logo_getor.png"
-            alt="logo getor"
-          />
-          <div>GEREJA TORAJA JEMAAT DEPOK</div>
+      <div
+        id="navbar"
+        className="py-3 px-5 sticky w-full font-mono font-semibold top-0 z-50 backdrop-blur-lg shadow-md rounded-b-3xl transition-all duration-300"
+      >
+        <div className="flex justify-between items-center">
+          <div className="">
+            <Logo />
+          </div>
+          <div className="justify-center hidden md:block">
+            <Nav dir="horizontal" scheme="light" />
+          </div>
+          <div className="md:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              class="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              stroke-width="2"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="M4 6h16M4 12h16M4 18h16"
+              />
+            </svg>
+          </div>
         </div>
-        <div>MENU</div>
-      </nav>
+      </div>
     </>
   );
 }
-
-export default Navbar;
