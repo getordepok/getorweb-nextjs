5c1cd0e06d7bc9dfc10c2dd7db34a2df74e294e7
Alrynto <48040209+oogenesa@users.noreply.github.com>
2022-06-24 00:07:04 +0700

    adjust flex birthday

--- components/Birthday.js
+++ components/Birthday.js
@@ -20,49 +20,58 @@ const Birthday = () => {
     {
       image: "images/rinto.jpg",
       name: "pnt. enov tikupasang",
-      age: "40",
+      age: "10",
     },
     {
       image: "images/rinto.jpg",
       name: "pnt. enov tikupasang",
-      age: "70",
+      age: "1",
     },
     {
       image: "images/rinto.jpg",
       name: "pnt. enov tikupasang",
-      age: "100",
+      age: "30",
+    },
+    {
+      image: "images/rinto.jpg",
+      name: "pnt. enov tikupasang",
+      age: "30",
     },
   ];
   return (
     <section>
-      <h1 className="text-2xl font-semibold font-mono text-center">
-        JEMAAT YANG BERULANG TAHUN HARI INI
-      </h1>
-      <h4 className="text-2 font-light font-mono text-center">
-        AJARLAH KAMI MENGHITUNG HARI-HARI KAMI SEDEMIKIAN, HINGGA KAMI BEROLEH
-        HATI YANG BIJAKSANA. MAZMUR 90:12
-      </h4>
-      <h4 className="text-2 font-semibold font-mono text-center">
-        SEGENAP MAJELIS GEREJA DAN ANGGOTA GERAJA TORAJA JEMAAT DEPOK
-        MENGUCAPKAN SELAMAT ULANG TAHUN, TUHAN MEMBERKATI
-      </h4>
-      <div className="bg-white shadow-skill rounded-md p-6 flex items-center">
-        <div className="flex flex-col md:flex-row lg:flex-row justify-around mx-auto">
-          {warta.map((wrt) => {
-            return (
-              <div className="bg-slate-100 mx-2 flex flex-col justify-center items-center rounded-xl shadow-md hover:bg-blue-500 ">
-                <img
-                  src={wrt.image}
-                  className=" object-none rounded-t-xl max-h-96 max-w-10 w-48"
-                />
+      <div className=" bg-slate-100 mx-10 mt-10">
+        <h1 className="text-2xl font-semibold font-mono text-center">
+          JEMAAT YANG BERULANG TAHUN HARI INI
+        </h1>
+        <h4 className="text-2 font-light font-mono text-center my-5">
+          AJARLAH KAMI MENGHITUNG HARI-HARI KAMI SEDEMIKIAN, HINGGA KAMI BEROLEH
+          HATI YANG BIJAKSANA. MAZMUR 90:12
+        </h4>
+        <h4 className="text-2 font-semibold font-mono text-center">
+          SEGENAP MAJELIS GEREJA DAN ANGGOTA GERAJA TORAJA JEMAAT DEPOK
+          MENGUCAPKAN SELAMAT ULANG TAHUN, TUHAN MEMBERKATI
+        </h4>
+        <div className=" shadow-skill rounded-md p-6">
+          <div className="flex flex-wrap items-center justify-center mx-20 ">
+            {warta.map((wrt) => {
+              return (
+                <div className="bg-slate-100 mx-4 mt-5 max-h-50 max-w-10 w-40 flex flex-col justify-center items-center rounded-xl">
+                  <img
+                    src={wrt.image}
+                    className=" object-none rounded-t-xl max-h-50 max-w-10 w-40"
+                  />
 
-                <h5>{wrt.name.toUpperCase()}</h5>
-                <p className="text-sm text-gray-400 font-semibold mt-1">
-                  {wrt.age} tahun
-                </p>
-              </div>
-            );
-          })}
+                  <h5 className="text-sm font-bold mt-1">
+                    {wrt.name.toUpperCase()}
+                  </h5>
+                  <p className="text-sm text-gray-400 font-semibold mt-1">
+                    {wrt.age} tahun
+                  </p>
+                </div>
+              );
+            })}
+          </div>
         </div>
       </div>
     </section>
--- components/LiturgiKeluarga.js
+++ components/LiturgiKeluarga.js
@@ -4,14 +4,16 @@ import CardLiturgiKeluarga from "./CardLiturgiKeluarga";
 const LiturgiKeluarga = () => {
   return (
     <section>
-      <h1 className="text-2xl font-semibold font-mono text-center mt-5">
-        LITURGI KELUARGA
-      </h1>
-      <h4 className="text-2 font-semibold font-mono text-center">
-        TETAPI AKU DAN SEISI RUMAHKU, KAMI AKAN BERIBADAH KEPADA TUHAN! YOSUA
-        24:15
-      </h4>
-      <CardLiturgiKeluarga />
+      <div className=" bg-white mx-10">
+        <h1 className="text-2xl font-semibold font-mono text-center mt-10">
+          LITURGI KELUARGA
+        </h1>
+        <h4 className="text-2 font-semibold font-mono text-center">
+          TETAPI AKU DAN SEISI RUMAHKU, KAMI AKAN BERIBADAH KEPADA TUHAN! YOSUA
+          24:15
+        </h4>
+        <CardLiturgiKeluarga />
+      </div>
     </section>
   );
 };
